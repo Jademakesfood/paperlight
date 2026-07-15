029051f5f2deca9d4635c7d4518fd6b194be8f36
Jademakesfood <sweetyiyi814@yahoo.com.hk>
2026-07-16 02:31:30 +0800

    Refresh Paperlight scanner cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'paperlight-v6';
+const CACHE = 'paperlight-v7';
 const CORE = ['./', './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
